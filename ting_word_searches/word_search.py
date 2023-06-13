37cc29807f3116dd3d130dd83f2a68a1ee5730da
Thiago Lira <thiago.slira@outlook.com>
2023-06-13 17:49:06 -0300

    Projeto finalizao

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -28,4 +28,27 @@ def exists_word(word: str, instance: Queue):
 
 
 def search_by_word(word: str, instance: Queue):
-    """Aqui irá sua implementação"""
+    word_lowed = word.lower()
+
+    found_files = []
+
+    for index_file in range(len(instance)):
+        actual_file = instance.search(index_file)
+        file_found = {
+            "palavra": word,
+            "arquivo": actual_file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for index_row in range(len(actual_file["linhas_do_arquivo"])):
+            line_lowed = actual_file["linhas_do_arquivo"][index_row].lower()
+            if word_lowed in line_lowed:
+                file_found["ocorrencias"].append(
+                    {
+                        "linha": index_row + 1,
+                        "conteudo": actual_file["linhas_do_arquivo"][index_row]
+                    }
+                )
+        if file_found["ocorrencias"]:
+            found_files.append(file_found)
+
+    return found_files
