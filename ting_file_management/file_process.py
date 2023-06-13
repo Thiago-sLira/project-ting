e97904b8e66dc1cd5df6ce2fe6e4fd298e8f40fc
Thiago Lira <thiago.slira@outlook.com>
2023-06-13 15:16:44 -0300

    Ajustado verificação se arquivo já existe na fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,11 +5,19 @@ from file_management import txt_importer
 
 def process(path_file, instance: Queue):
     data_file = txt_importer(path_file)
+
     new_dict = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(data_file),
-        "linhas_do_arquivo": data_file
+        "linhas_do_arquivo": data_file,
     }
+
+    for index in instance.__len__():
+        if path_file == instance.search(index)["nome_do_arquivo"]:
+            pass
+        else:
+            instance.enqueue(new_dict)
+            sys.stdout.write(new_dict)
 
 
 def remove(instance):
