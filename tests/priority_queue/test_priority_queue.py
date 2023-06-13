7f7fc07687fadc5620409c1adaa78b6423ce3a42
Thiago Lira <thiago.slira@outlook.com>
2023-06-13 16:33:39 -0300

    Criando duas variáveis para mockar o teste

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,19 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    new_regular_customer = {
+        "nome_do_arquivo": "Fulado de Tal",
+        "qtd_linhas": 3,
+        "linhas_do_arquivo": ["Um", "cliente", "regular"],
+    }
+
+    new_priority_customer = {
+        "nome_do_arquivo": "Ciclano de Tal",
+        "qtd_linhas": 6,
+        "linhas_do_arquivo": ["Um", "cliente", "prioritário", "está", "presente"],
+    }
+
+    priority_queue = PriorityQueue()
+
+    # priority_queue.enqueue()
