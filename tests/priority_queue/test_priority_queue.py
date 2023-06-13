46e17a6cdf44f9e5e8310a1bb683068ecf7aeb5f
Thiago Lira <thiago.slira@outlook.com>
2023-06-13 16:58:56 -0300

    Testes para priority_queue concluídos

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,4 +1,5 @@
 from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
@@ -22,6 +23,14 @@ def test_basic_priority_queueing():
     priority_queue.enqueue(new_priority_customer)
     assert len(priority_queue) == 2
 
+    assert priority_queue.search(0)["qtd_linhas"] == 3
+    assert priority_queue.search(1)["qtd_linhas"] == 6
+
     priority_queue.dequeue()
     assert len(priority_queue.high_priority) == 0
     assert len(priority_queue.regular_priority) == 1
+
+    with pytest.raises(IndexError):
+        priority_queue.search(500)
+
+    # priority_queue.enqueue(new_regular_customer)
