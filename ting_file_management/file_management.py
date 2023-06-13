8846057295dc832c7db083d314a89cb64cf8eb5e
Thiago Lira <thiago.slira@outlook.com>
2023-06-13 14:33:32 -0300

    Função txt_importer concluída

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,4 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    with open(path_file, "r") as file:
-        for line in file:
-            return line
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
