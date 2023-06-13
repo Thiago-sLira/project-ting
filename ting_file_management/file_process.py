399c8c8c63994f522d00f9349d014c3246e856f4
Thiago Lira <thiago.slira@outlook.com>
2023-06-13 15:03:00 -0300

    Criado um dicionario para armazenar o novo elemento da fila

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,15 @@
+import sys
 from queue import Queue
 from file_management import txt_importer
 
 
 def process(path_file, instance: Queue):
     data_file = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data_file),
+        "linhas_do_arquivo": data_file
+    }
 
 
 def remove(instance):
